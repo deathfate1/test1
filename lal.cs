b9af60969c2815ca116d32b8770d3da288360467
Vlad <oskolkovlad>
2019-10-01 21:14:51 +0300

    First fs

---
+++ lal.cs
@@ -0,0 +1,25 @@
+private class Classes{
+    private benfdffd;
+
+    dsfsdfsfsdfsfsf;
+    sdfsdfsdfsdfsd;
+    sdfdsfdsfsdf;
+
+
+
+    sdfsdfdsfsdfsf;
+    sdfsdfsdfsdf;dsfsdfsfsdfsfsf
+
+
+    sdfsdfsfsdf;
+
+
+
+    sbgdfs
+
+    ;sfdsfsfsd;
+
+
+    sdfdsfdsf;
+    sdfsdf
+}
